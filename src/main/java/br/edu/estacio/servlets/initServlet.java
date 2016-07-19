571b9c1e1bfd89ee5bf42bbc57063059f0a0e5b8
welder496 <welder496@gmail.com>
2016-07-19 16:02:19 -0300

    Inclusão do bootstrap e arquivos jsp

--- src/main/java/br/edu/estacio/servlets/initServlet.java
+++ src/main/java/br/edu/estacio/servlets/initServlet.java
@@ -29,24 +29,6 @@ public class initServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		PrintWriter pw = response.getWriter();
-		pw.println("<html>");
-		pw.println("<head>");
-		pw.println(" <title>Cadastro</title>");
-		pw.println("</head>");
-		pw.println("<body>");
-		pw.println("<form action='/servletApp/cadastrar' method='POST'>");
-		pw.println("<div>");
-		pw.println("<label>Nome: </label>");
-		pw.println("<input type='text' name='nome'/>");
-		pw.println("</div>");
-		pw.println("<div>");
-		pw.println("<input type='submit' value='Enviar'/>");
-		pw.println("</div>");
-		pw.println("</form>");
-		pw.println("</body>");
-		pw.println("</html>");
-		pw.close();
 		
 	}
 
@@ -55,21 +37,7 @@ public class initServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		PrintWriter pw = response.getWriter();
-		pw.println("<html>");
-		pw.println("<head>");
-		pw.println(" <title>Nome cadastrado</title>");
-		pw.println("</head>");
-		pw.println("<body>");
-		pw.println("<div>");
-		pw.println("<p>Nome digitado: "+request.getParameter("nome"));
-		pw.println("</div>");		
-		pw.println("<div>");
-		pw.println("<a href='/servletApp/'>Voltar para o início</a>");
-		pw.println("</div>");
-		pw.println("</body>");
-		pw.println("</html>");
-		pw.close();
+
 	}
 
 }
