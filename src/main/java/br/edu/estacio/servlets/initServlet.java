913b6e37c337c1e6edf0466b9f12174d7cee6062
welder496 <welder496@gmail.com>
2016-07-19 17:10:52 -0300

    Atualização do projeto

--- src/main/java/br/edu/estacio/servlets/initServlet.java
+++ src/main/java/br/edu/estacio/servlets/initServlet.java
@@ -29,15 +29,17 @@ public class initServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+		response.sendRedirect("/servletApp/cadastrar.jsp");
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-
+		String nome = request.getParameter("nome");
+		//Sending to expression language ${nome} via session 
+		request.getSession().setAttribute("nome", nome);
+		response.sendRedirect("/servletApp/mostrarNome.jsp");
 	}
 
 }
